4e88a00046a1882020b18c8b5dd1f54dc2eb0aca
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-11-09 21:43:04 +0100

    Change game name + color navbar

--- client/components/Header.js
+++ client/components/Header.js
@@ -51,7 +51,7 @@ class Header extends Component {
     return (
 
       <AppBar
-        title="MemoryGame"
+        title="Dots&Boxes"
         iconElementLeft={ <IconButton onClick={ this.navigateHome }><ActionHome /></IconButton> }
         iconElementRight={ this.renderMenu() }
       />
