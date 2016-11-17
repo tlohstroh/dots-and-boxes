8f87cc9c5722e0589a49fa03b268497be6707886
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-11-17 13:21:32 +0100

    Add: console knows if a box iw won :-)

--- src/services/game/hooks/check-winner.js
+++ src/services/game/hooks/check-winner.js
@@ -7,21 +7,44 @@
 
 const defaults = {};
 
+// function getMatches(array){
+//   const takenEdgesIds = hook.data.takenEdgesIds;
+//   return array.filter((id) => )
+// };
+
+
 
 module.exports = function(options) {
   options = Object.assign({}, defaults, options);
 
-
-
-
-
   return function(hook) {
-    const turn = hook.data.turn
+    const turn = hook.data.turn;
+    const boxes = hook.data.boxes;
+    const edges = hook.data.edges;
+    const takenEdgesIds = hook.data.takenEdgesIds;
+    const clickedEdgeId = takenEdgesIds[(takenEdgesIds.length)-1];
 
-    if(false){
-      //some code do see if there is a winner
+
+
+    // filter out the boxes that have pickedEdge in them
+    // this will give can array of one or two objects!
+    const matchingBoxes = boxes.filter(box => box.boxEdges.indexOf(clickedEdgeId) !== -1);
+    const matchingBoxOneEdges = matchingBoxes[0].boxEdges
+
+
+    // check for box 1
+    // if(checkWinner(matchingBoxes[0].boxEdges) === true){
+    //   console.log("a box is won!!")
+    // }
+    if (matchingBoxOneEdges.filter(id => takenEdgesIds.indexOf(id) !== -1).length === 4){
+      console.log("Hallelujah!! A box is won!!!");
     }
     else{
+      console.log("TAKEN EDGE-IDS: ", takenEdgesIds);
+      console.log("BOX EDGES box 1: ", matchingBoxes[0].boxEdges);
+      console.log("MATCHING BOXES: ", matchingBoxes.length);
+      console.log("No box is won...");
+      console.log("....................");
       let nextTurn = turn + 1
       if (nextTurn > 1){
         nextTurn = 0
